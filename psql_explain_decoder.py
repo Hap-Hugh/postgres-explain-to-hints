53ddfddd55d91e8d35a472730c239fa5fabbee15
Hap-Hugh <haphugh@gmail.com>
2023-04-03 18:15:07 +0000

    decoder for top-k plans from PG

--- psql_explain_decoder.py
+++ psql_explain_decoder.py
@@ -78,3 +78,116 @@ join_order, join_conds, single_scans = decode(plan['Plans'], plan['Node Type'])
 print(join_order)
 print(join_conds)
 print(single_scans)
+
+
+def start_with_tab(str, x):
+    tab_string = ""
+    for i in range(x):
+        tab_string = tab_string + "    "
+    if str.startswith(tab_string) and not str.startswith(tab_string + "    "):
+        return True
+    else:
+        return False
+
+
+JOIN_KEYWORD = ["MergeJoin", "HashJoin", "NestLoop"]
+SCAN_KEYWORD = ["SeqScan", "IdxScan"]
+
+
+join_kw_to_we_need = {
+    'MergeJoin' : 'Merge Join',
+    'HashJoin' : 'Hash Join',
+    'NestLoop' : 'Nested Loop'
+}
+
+scan_kw_to_we_need = {
+    "SeqScan": "Seq Scan",
+    "IdxScan": "Index Scan"
+}
+
+def pre_build_plan_tree(input_string):
+    lines = input_string.strip().split('\n')
+    return_lines = []
+    for i in range(len(lines)):
+        flag = False
+        for jkw in JOIN_KEYWORD:
+            if jkw in lines[i]:
+                flag = True
+                break
+        for sjw in SCAN_KEYWORD:
+            if sjw in lines[i]:
+                flag = True
+                break
+        if flag:
+            return_lines.append(lines[i])
+    return "\n".join(return_lines)
+
+
+
+### MergeJoin(MergeJoin(HashJoin(v, p), b), MergeJoin(c, u))
+
+def build_plan_tree(input_string, x):
+    # Split the input string into lines
+    lines = input_string.strip().split('\n')
+
+    # Extract the operator name and its arguments from the first line
+    operator_args = lines[0].split('(')[1].split(')')[0].split()
+    print(operator_args)
+    operator_name = lines[0].split('(')[0]
+    
+    if len(operator_args) == 1:
+        return operator_args[0]
+
+
+    left_start = False
+    right_start = False
+    left_lines = []
+    right_lines = []
+    for i in range(1, len(lines)):
+        if start_with_tab(lines[i], x+1) and not left_start:
+            left_start = True
+            right_start = False
+            left_lines.append(lines[i])
+            continue
+        if start_with_tab(lines[i], x+1) and left_start:
+            left_start = False
+            right_start = True
+            right_lines.append(lines[i])
+            continue
+        if left_start:
+            left_lines.append(lines[i])
+            continue
+        if right_start:
+            right_lines.append(lines[i])
+            continue
+
+    left_result = build_plan_tree("\n".join(left_lines), x+1)
+    right_result = build_plan_tree("\n".join(right_lines), x+1)
+
+    return join_kw_to_we_need[operator_name] + "(" + left_result + "," + right_result + ")"
+
+
+def build_scan_methods_set(lines):
+    result = set()
+    lines = input_string.strip().split('\n')
+    for i in range(0, len(lines)):
+        for k in SCAN_KEYWORD:
+            if k in lines[i]:
+                operator_args = lines[i].split('(')[1].split(')')[0].split()
+                print(operator_args)
+                operator_name = lines[i].split('(')[0].split()[0]
+                result.add(scan_kw_to_we_need[operator_name] + "(" + operator_args[0] + ")")
+    return result
+
+
+# Read the input from "tmp_plan.txt"
+with open("tmp_plan.txt") as f:
+    input_string = f.read()
+
+# Build the plan tree and print the serialized plan
+preprocessed_string = pre_build_plan_tree(input_string)
+print(preprocessed_string)
+scan_method_set = build_scan_methods_set(preprocessed_string)
+print(scan_method_set)
+serialized_plan = build_plan_tree(preprocessed_string, 1)
+print(serialized_plan)
